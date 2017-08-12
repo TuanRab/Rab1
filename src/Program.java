848e64564ebe04d6425acafada3a3017e350b73a
TuanRab <vuanhtuanptit@gmail.com>
2017-08-12 20:02:42 +0700

    Added BoxCollider

--- src/Program.java
+++ src/Program.java
@@ -1,4 +1,6 @@
 import bases.GameObject;
+import bases.physics.BoxCollider;
+import touhou.BackGrounds.BackGround;
 import touhou.GameWindow;
 
 import java.util.ArrayList;
@@ -11,5 +13,10 @@ public class Program {
     public static void main(String[] args) {
         GameWindow gameWindow = new GameWindow();
         gameWindow.loop();
+
+//        BoxCollider a = new BoxCollider(0, 0, 30, 30);
+//        BoxCollider b = new BoxCollider(0, 0, 60, 60);
+//
+//        System.out.println(a.intersects(b));
     }
 }
